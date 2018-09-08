e6b20ff53ee43d094f112c9dcb51d0bfcfbd3971
Warif Akhand Rishi <rishi420@gmail.com>
2018-09-08 19:22:14 +0600

    notification button action work in progress

--- Example/CustomSizeController/ViewController.swift
+++ Example/CustomSizeController/ViewController.swift
@@ -17,6 +17,8 @@ private enum TransitionType {
 }
 
 // TODO:- Test with build in view controlers mailVC, SMSVC, VideoPlayerVC
+// TODO:- Rename PopTransition to BubbleTransition
+// TODO:- Pop animation origin after rotation
 
 class ViewController: UIViewController {
     
@@ -28,42 +30,98 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    // Code
-    @IBAction func ShowSmallVCButtonAction(_ sender: Any) {
-
-        transitionType = .slide(fromDirection: .right)
+    // MARK: Show Small View controller
+    private func showSmallVC(transition: TransitionType) {
+        
+        transitionType = transition
         
         let vc = storyboard?.instantiateViewController(withIdentifier: "SmallVC") as! SmallViewController
         vc.view.backgroundColor = .red
         vc.transitioningDelegate = self
         vc.modalPresentationStyle = .custom
         present(vc, animated: true, completion: nil)
-        
-        // Dismiss after delay
-        // customSizeC?.perform(#selector(customSizeC?.dismiss), with: nil, afterDelay: 5)
-        // TODO: NOTIFICAITON
     }
     
-    // Popup
-    @IBAction func showAsPopupButtonAction(_ sender: Any) {
-        
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SmallVC") as! SmallViewController
-        CustomSizePopupUtility.shared.show(viewController: vc)
-    }
-    
-    // Storyboard
+    // MARK: Storyboard
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.destination is SmallViewController {
             transitionType = .slide(fromDirection: .down)
             segue.destination.transitioningDelegate = self
             segue.destination.modalPresentationStyle = .custom
         }
     }
+}
+
+// MARK:- Button Actions
+extension ViewController {
     
+    // MARK: Slide in
+    @IBAction func leftButtonAction(_ sender: Any) {
+        print("leftButtonAction")
+        showSmallVC(transition: .slide(fromDirection: .left))
+    }
+    
+    @IBAction func rightButtonAction(_ sender: Any) {
+        print("rightButtonAction")
+        showSmallVC(transition: .slide(fromDirection: .right))
+    }
+    
+    @IBAction func topButtonAction(_ sender: Any) {
+        print("topButtonAction")
+        showSmallVC(transition: .slide(fromDirection: .up))
+    }
+    
+    @IBAction func bottomButtonAction(_ sender: Any) {
+        print("bottomButtonAction")
+        showSmallVC(transition: .slide(fromDirection: .down))
+    }
+    
+    // MARK: Menu Buttons
+    @IBAction func leftMenuButtonAction(_ sender: Any) {
+        print("leftMenuButtonAction")
+        showSmallVC(transition: .menu(fromDirection: .left))
+    }
+    
+    @IBAction func rightMenuButtonAction(_ sender: Any) {
+        print("rightMenuButtonAction")
+        showSmallVC(transition: .menu(fromDirection: .right))
+    }
+    
+    // MARK: Popup Button
+    @IBAction func showAsPopupButtonAction(_ sender: Any) {
+        
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SmallVC") as! SmallViewController
+        CustomSizePopupUtility.shared.show(viewController: vc)
+    }
+    
+    // MARK: Notification Button
+    @IBAction func notificationButtonAction(_ sender: Any) {
+        print("notificationButtonAction")
+        
+        transitionType = .slide(fromDirection: .right)
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "SmallVC") as! SmallViewController
+        
+        vc.transitioningDelegate = self
+        vc.modalPresentationStyle = .custom
+    
+        // Dismiss after delay
+        vc.perform(#selector(vc.dismiss(animated:completion:)), with: nil, afterDelay: 2)
+        
+        present(vc, animated: true, completion: nil)
+    }
+    
+    // MARK: Bubble Button
+    @IBAction func bubbleButtonAction(_ sender: Any) {
+        print("bubbleButtonAction")
+        showSmallVC(transition: .bubble)
+    }
+    
+    // MARK: Unwind Segue
     @IBAction func unwind(sender: UIStoryboardSegue) {}
 }
 
+// MARK:- CustomSizeControllerDelegate
 extension ViewController: CustomSizeControllerDelegate {
     
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
@@ -87,8 +145,12 @@ extension ViewController: CustomSizeControllerDelegate {
             return CGRect(origin: CGPoint(x: 0, y: containerViewFrame.height / 4), size: CGSize(width: containerViewFrame.width, height: containerViewFrame.height / (4/3)))
         case .bubble:
             return CGRect(origin: CGPoint(x: 0, y: containerViewFrame.height / 4), size: CGSize(width: containerViewFrame.width, height: containerViewFrame.height / 2))
-        case .menu:
-            return CGRect(origin: .zero, size: CGSize(width: containerViewFrame.width / 2, height: containerViewFrame.height))
+        case .menu(let fromDirection):
+            var origin = CGPoint.zero
+            if fromDirection == .right {
+                origin = CGPoint(x: containerViewFrame.width / 2, y: 0)
+            }
+            return CGRect(origin: origin, size: CGSize(width: containerViewFrame.width / 2, height: containerViewFrame.height))
         }
     }
 }
