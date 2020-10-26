d47e68073cc53e83d9ffb6bd267be83628c41d2c
Warif Akhand Rishi <rishi420@gmail.com>
2020-10-26 23:13:05 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BonsaiController",
     platforms: [
-        .iOS(.v9.3)
+        .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
